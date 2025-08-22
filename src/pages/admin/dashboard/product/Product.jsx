0621586ce1df878ec8c85ac901b6ee12274daed4
Shristi Pokharel <shristipokharel889@gmail.com>
2025-08-22 15:18:11 +0545

    Implementing the code for editing the product

--- src/pages/admin/dashboard/product/Product.jsx
+++ src/pages/admin/dashboard/product/Product.jsx
@@ -1,106 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import { handleDeleteOperation, handleGetOperation } from '../../../../handleOperation/handleOperation'
-import { apiLinks } from '../../../../handleOperation/apiLinks'
-// import { useNavigate } from 'react-router-dom'
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { handleDeleteOperation, handleGetOperation } from "../../../../handleOperation/handleOpertion";
+import { apiLinks } from "../../../../handleOperation/apiLinks";
+import { useNavigate } from "react-router-dom";
 
 const Product = () => {
+  const [formData, setFormData] = useState([]);
+  const [count,setCount] = useState(1)
 
-  const [formData,setFormData] = useState([])
-  const [count , setCount] = useState(1)
+  const navigate = useNavigate()
 
-
-  useEffect(()=>{
-    const fetchProduct = async()=>{
-
+  useEffect(() => {
+    const fetchProduct = async () => {
       try {
-        
-        const result= await handleGetOperation(apiLinks.getAllProduct)
-   
-        if(result.status == 200){
-         
-         
-          setFormData(result.data.data)
+        console.log(apiLinks.getAllProduct);
+        const result = await handleGetOperation(apiLinks.getAllProduct);
+        if (result.status == 200) {
+          console.log(result.data.data);
+          setFormData(result.data.data);
         }
       } catch (error) {
-        console.error("Failed to fetch product:", error);
-        alert("Failed to fetch product");
+        console.log(error);
       }
+    };
+    fetchProduct();
+  }, [count]);
 
+  const handleDelete = async (id)=>{
+    const isConfirmed = confirm("This is going to be deleted")
+    if(isConfirmed){
+     
+      await handleDeleteOperation(apiLinks.deleteProduct(id))
+      setCount(count+1)
+    }else{
+      alert("Not Deleted")
     }
-    fetchProduct()
-  },[count])
-
-
-  const handledDelete = async(id)=>{
     
-    const isDelete = confirm("Do you want to delete ")
-    if(isDelete){
-      const result = await handleDeleteOperation(apiLinks.deleteProduct(id))
 
-      if(result.status == 200){
-        setCount((prev)=>prev+1)
-        // location.href = '/admin/product'
-      }
-    }
   }
+
 
   return (
     <div>
+      <table className="min-w-full bg-white border undefined">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 h-6 sticky top-0 z-10 transition-all ease-in-out ">
+          <tr>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200 w-8">S.N </th>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200 w-8">Image </th>
 
- <div className="flex flex-col overflow-y-auto h-full">
-  <table className="min-w-full bg-white border undefined">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-50 h-6 sticky top-0 z-10 transition-all ease-in-out ">
-      <tr>
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200 w-8" />
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200">Project</th>
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200">Brand</th>
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200">Action</th>
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200">State</th>
-        <th className="px-2 2xl:px-6 py-3 bg-gray-200 w-6" />
-      </tr>
-    </thead>
-    <tbody className="undefined select-none">
-
-    {formData.map((product)=>(
-      <tr className="border-b" key={product._id}>
-       
-       
-        <td className="px-6 py-1.5 whitespace-nowrap text-sm">
-          <div className="flex items-center gap-2">
-            <img src={product.imageUrl} alt="image" className="aspect-square rounded-lg object-cover h-16" />
-            <p className="font-bold">{product.productName}</p>
-          </div>
-        </td>
-        <td className="px-6 py-1.5 whitespace-nowrap text-sm  text-center">{product.brand}</td>
-        <td className="px-6 py-1.5 whitespace-nowrap text-sm   flex justify-center items-center h-full gap-2">
-        <td className="px-6 py-1.5 whitespace-nowrap text-sm  text-center">
-          <button className="text-xs font-bold me-2 px-2.5 shadow py-2  bg-green-200 text-green-600 font-semibold rounded-lg shadow">edit</button>
-          <button onClick={()=>(handledDelete(product._id))} className="text-xs font-bold me-2 px-2.5 shadow py-2  bg-red-500 text-white font-semibold rounded-lg shadow">delete</button>
-        </td>
-          <div className="relative inline-block text-left select-none ">
-            <div className="rounded-full py-4 hover:bg-gray-100 focus:outline- cursor-pointer "><svg xmlns="http://www.w3.org/2000/svg" width={26} height={26} viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2">
-                </path>
-              </svg></div>
-          </div>
-        </td>
-      </tr>
-    ))}
-
-      
-    
-    
-     
-   
-     
-  
-    </tbody>
-  </table>
-</div>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200">Product Name</th>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200">Brand</th>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200">Created At</th>
+            <th className="px-2 2xl:px-6 py-3 bg-gray-200">Action</th>
+            {/* <th className="px-2 2xl:px-6 py-3 bg-gray-200 w-6" /> */}
+          </tr>
+        </thead>
+        <tbody className="undefined select-none">
 
 
+
+          {formData.map((product,index) => (
+            <tr className="border-b" key={product._id}>
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm">{index + 1}</td>
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={product.imageUrl}
+
+                    alt="image"
+                    className="aspect-square rounded-lg object-cover h-16"
+                  />
+                </div>
+              </td>
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm  text-center">
+                {product.productName}
+              </td>
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm  text-center">
+               {product.brand}
+              </td>
+
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm   flex justify-center items-center h-full gap-2">
+                <div className="relative inline-block text-left select-none ">
+                  <div className="rounded-full py-4 hover:bg-gray-100 focus:outline- cursor-pointer ">
+                    <p>{product.price}</p>
+                  </div>
+                </div>
+              </td>
+              <td className="px-6 py-1.5 whitespace-nowrap text-sm  text-center">
+                <button onClick={()=>{navigate(`/admin/editProduct/${product._id}`)}} className="text-xs font-bold me-2 px-2.5 shadow py-2  bg-green-200 text-green-600 font-semibold rounded-lg shadow">
+                  Edit
+                </button>
+                <button onClick={()=>(handleDelete(product._id))} className="text-xs font-bold me-2 px-2.5 shadow py-2  bg-red-400 text-white font-bold rounded-lg shadow">
+                  Delete
+                </button>
+              </td>
+            </tr>
+
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
